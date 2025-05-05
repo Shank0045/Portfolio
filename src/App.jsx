d56a4bf68101db5059dc616add328d398a0a0d33
shashank <shashank10733@gmail.com>
2025-05-05 20:02:18 +0530

    made some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
 
-import React, { useContext } from 'react'
+import React, { useContext, useRef } from 'react'
 import Intro from './components/intro'
 import { Route, Routes } from 'react-router-dom'
 import Project from './components/Project'
 import About from './components/About'
 import Contact from './components/Contact'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 
 
 const App = () => {
+
+  const div = useRef(null);
+
+  useGSAP(()=>{
+
+    gsap.from(div.current,{
+       scale:0,
+       opacity:0,
+       duration:1.5
+      
+    })
+
+
+  })
+
+
 
 
 
   return (
         <>
 
-<div className='w-full  min-h-screen max-h-max  text-[#FFFFFF] bg-[#1A1A1A]'>
+<div ref={div} className='w-full  min-h-screen max-h-max  text-[#FFFFFF] bg-[#1A1A1A]'>
 <Routes>
 <Route path='/' element={<Intro/>} />
 <Route path='/project' element={<Project/>} />
