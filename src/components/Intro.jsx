164b25c1665fbc388a6779779405ab31d184df8e
shashank <shashank10733@gmail.com>
2025-05-07 11:17:59 +0530

    updated

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,10 +1,6 @@
 
-import { useEffect, useRef } from 'react';
 import Nav from './Nav'
-import { useGSAP } from '@gsap/react';
-import gsap from 'gsap';
-import { FaReact } from "react-icons/fa";
-import { SiTailwindcss } from "react-icons/si";
+
 
 const Intro = () => {
 
@@ -29,11 +25,7 @@ const Intro = () => {
    <div className=' mt-[50px] flex items-center justify-center'>
     <div className=' flex items-center justify-around flex-col w-[93vw] max-h-max  min-h-[80vh] rounded-[50px] nav'>
    <h1 className=' text2 text-2xl md:text-4xl lg:text-5xl pt-14 font-[200]'> 👋 Hi, I’m Shashank  </h1>
-<p className=' md:w-[70%] w-[85%] lg:h-[70%] h-[50%]  font-[200] text-2xl md:text-2xl pt-15 lg:text-3xl text-center leading-[45px]  md:leading-[59px]' >A self-taught frontend developer skilled in React <span><FaReact className=' text-[40px] text-blue-500 inline' /></span>   ,Tailwind CSS <span><SiTailwindcss className=' text-[40px] text-blue-500  inline' /></span>   , and GSAP <span> <img className=' w-[59px] h-[59px] rounded-[50%] text-blue-500 inline' src="gsap.jpg" alt="" /> </span>    . I build fast, responsive, and interactive web interfaces that focus on clean code and user experience. Open to roles where I can solve real problems and grow with a creative tech team.</p>
-
-
-
-
+<p className=' md:w-[70%] w-[85%] lg:h-[70%] h-[50%]  font-[200] text-2xl md:text-2xl pt-15 lg:text-3xl text-center leading-[45px]  md:leading-[59px]' >A self-taught frontend developer skilled in React ,Tailwind CSS,and GSAP. I build fast, responsive, and interactive web interfaces that focus on clean code and user experience. Open to roles where I can solve real problems and grow with a creative tech team.</p>
 
     </div>
 
